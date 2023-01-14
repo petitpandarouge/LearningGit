e5a74fc7ed26825fe721d841cb2ac797ed421367
Damien Porté <>
2023-01-14 08:45:53 +0100

    fix: #4
    fix: Correction exercice 1.
    fix: Correction exercice 2

--- Rebase/Exercice1/init.sh
+++ Rebase/Exercice1/init.sh
@@ -12,13 +12,13 @@ echo "US2" >> us2.txt
 git add us2.txt
 git commit -m "feat: US2"
 git checkout main
-git branch feature/US3
-git checkout feature/US3
+git branch feat/US3
+git checkout feat/US3
 touch us3.txt
 echo "commit 1" >> us3.txt
 git add us3.txt
-git commit -m "commit 1"
+git commit -m "feat: commit 1"
 echo "commit 2" >> us3.txt
 git add us3.txt
-git commit -m "commit 2"
+git commit -m "feat: commit 2"
 git checkout main
--- Rebase/Exercice2/init.sh
+++ Rebase/Exercice2/init.sh
@@ -3,24 +3,24 @@
 touch program.txt
 echo "main" >> program.txt
 git add program.txt
-git commit -m "init"
+git commit -m "chore: init"
 git tag 1.0.0
 git branch SP1
-git branch feature/US1
-git branch feature/US2
-git checkout feature/US1
+git branch feat/US1
+git branch feat/US2
+git checkout feat/US1
 touch us1.txt
 echo "commit 1" >> us1.txt
 git add us1.txt
-git commit -m "commit 1"
+git commit -m "feat: commit 1"
 echo "commit 2" >> us1.txt
 git add us1.txt
-git commit -m "commit 2"
-git checkout feature/US2
+git commit -m "feat: commit 2"
+git checkout feat/US2
 touch us2.txt
 echo "commit 3" >> us2.txt
 git add us2.txt
-git commit -m "commit 3"
+git commit -m "feat: commit 3"
 echo "commit 4" >> us2.txt
 git add us2.txt
-git commit -m "commit 4"
+git commit -m "feat: commit 4"
